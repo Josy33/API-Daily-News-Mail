191f1e16e249e1ac82d25cb37a9ef776c4309f85
Jojo <ephraimjosiahotto@gmail.com>
2024-10-07 23:02:08 +0100

    add links, specified language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "92396e6562b4436eb3f1184c726e084a"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2024-09-04&sortBy=publishedAt&apiKey=923" \
-      "96e6562b4436eb3f1184c726e084a"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=92396e6562b4436eb3f1184c726e084a" \
+      "&language=en"
+
 
 # Make a request
-requests = requests.get(url)
+response = requests.get(url)
 
 # Get a dictionary with data
-content = requests.json()
+content = response.json()
 
 # Access the article titles and description
-body = ""
-for article in content["articles"]:
-    title = article["title"]
-    description = article["description"] or ""  # Ensures desciption is a string
-    if title is not None:
-        body += title + "\n" + description + 2 * "\n"
-        # if article["title"] is not None:
-        #   body = body + article["title"] + "n" + article["description"] + 2*"\n"
+body = "Subject: Today's news\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body += article["title"] + "\n" \
+               + (article["description"] or "") + "\n" \
+               + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
